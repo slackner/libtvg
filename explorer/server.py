3d39c9ebc39d580c01bcc8b73eb1bc0fb9ce785e
Sebastian Lackner <sebastian@fds-team.de>
2019-07-05 15:06:08 +0200

    explorer: Improve the 'stable_topics' metric.
    Use the more complicated topics measure only to determine the importance.
    The standard deviation is computed based on normalized sum graphs.

--- explorer/server.py
+++ explorer/server.py
@@ -139,13 +139,12 @@ class Client(WebSocket):
             subgraph = graph.sparse_subgraph()
 
         elif self.context['edgeWeight'] == 'stable_topics':
-            graphs = dataset_tvg.sample_graphs(ts_min, ts_max, sample_width=(ts_max - ts_min) / 3, method='topics')
+            graphs = dataset_tvg.sample_graphs(ts_min, ts_max, sample_width=(ts_max - ts_min) / 3)
             graphs = [g.normalize() for g in graphs]
-            graph = pytvg.metric_stability_pareto(graphs, base=0.5)
+            topics = dataset_tvg.topics(ts_min, ts_max)
+            graph = pytvg.metric_stability_pareto(graphs, mean=topics, base=0.5)
             seeds, _ = graph.top_edges(8, ret_weights=False)
-
-            graph = dataset_tvg.topics(ts_min, ts_max)
-            subgraph = graph.sparse_subgraph(seeds=seeds)
+            subgraph = topics.sparse_subgraph(seeds=seeds)
             for i, j in seeds:
                 edge_colors[i, j] = "red"
 
